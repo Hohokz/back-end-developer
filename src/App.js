0d4306654c4bbc8ed3cb6316e44ffeeea7f95423
Apiwat.S <hohokz@hotmail.com>
2022-12-27 15:22:27 +0700

    feat : landing page and header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './component/header';
+import Body from './component/body';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </header>
-    </div>
+    <>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+      </style>
+      <Header />
+      <Body />
+    </>
   );
 }
 
---
+++ src/component/body.js
@@ -0,0 +1,10 @@
+function Body() {
+    return (
+        <div className="flex box-border justify-center bg-sky-300 ">
+            <div className="w-[80em] h-[50em] bg-white rounded-t-[15px] shadow-[0_-5px_10px_rgba(0,0,0,0.07)]">
+            </div>
+        </div>
+    )
+}
+
+export default Body;
